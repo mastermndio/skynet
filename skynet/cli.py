35c19a0cba574be1d6042c917b327666ea3f8572
Aaron Brooks <abrooks@mastermnd.io>
2020-09-24 21:18:44 -0400

    Added Farenheit to Celcius converter

--- skynet/cli.py
+++ skynet/cli.py
@@ -1,5 +1,15 @@
 import click
+import platform
 
 @click.group()
 def cli():
-   """ Welcome to Skynet """ 
+    """ Welcome to Skynet """
+
+@cli.command()
+@click.option('--temp', type=int, help='The temperature you want to convert')
+@click.option('--units', help='The units of your temperatur and what you want it converted to')
+def convert(temp, units):
+    if units == 'f2c':
+        new_temp = (temp - 32) * 5/9
+        print(f'{temp} degreed F is equal to {new_temp} degrees C')
+
